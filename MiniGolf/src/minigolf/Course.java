44c4f2c0179350f4e4c064c3c70f2e50cea222ca
Jason Andrew Kerby <jason.a.kerby@gmail.com>
2015-05-07 06:15:17 +0900

    Modifying Course to accept a Golf.txt file which provides information on the course -Kerb

--- MiniGolf/src/minigolf/Course.java
+++ MiniGolf/src/minigolf/Course.java
@@ -5,28 +5,52 @@
  */
 package minigolf;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author John Silvey
  */
 public class Course {
-    
-    public static String getName(){
-        //This method returns the course name
-        String courseName = "name";
+
+    public static String getName() {
+        //This method returns the course name, address, and zip code
+        File f = new File("Golf.txt");
+        StringBuilder sb = new StringBuilder();
+        try {
+            Scanner s = new Scanner(f);
+            for(int i=0; i < 3; i++){
+                sb.append(s.nextLine() + "\n");
+            }
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
+
+        String courseName = sb.toString();
         return courseName;
     }
-    
-    public static int getNumberOfHoles(){
+
+    public static int getNumberOfHoles() {
         //This method returns the number of holes on the course
-        int numOfHoles=18;
+        int numOfHoles = 0;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("Golf.txt"));
+            while (reader.readLine() != null){
+                numOfHoles ++;
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error!");
+        }
+        numOfHoles = numOfHoles-3;
         return numOfHoles;
     }
-    
-    public static int parForHole( int hole ){
+
+    public static int parForHole(int hole) {
         //This method returns the par number for the requested hole
         int par = 4;
         return par;
     }
-    
+
 }
