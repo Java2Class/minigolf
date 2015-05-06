8f277c5b97332905294205f2a51f66057e57e7b0
Jason Andrew Kerby <jason.a.kerby@gmail.com>
2015-05-07 07:14:55 +0900

    Finished Course.parForHole.  Makes a HashMap and returns an int value of the par for the provided hole -Kerb

--- MiniGolf/src/minigolf/Course.java
+++ MiniGolf/src/minigolf/Course.java
@@ -20,7 +20,7 @@ public class Course {
         StringBuilder sb = new StringBuilder();
         try {
             Scanner s = new Scanner(f);
-            for(int i=0; i < 3; i++){
+            for (int i = 0; i < 3; i++) {
                 sb.append(s.nextLine() + "\n");
             }
         } catch (Exception e) {
@@ -34,22 +34,38 @@ public class Course {
     public static int getNumberOfHoles() {
         //This method returns the number of holes on the course
         int numOfHoles = 0;
-        try{
+        try {
             BufferedReader reader = new BufferedReader(new FileReader("Golf.txt"));
-            while (reader.readLine() != null){
-                numOfHoles ++;
+            while (reader.readLine() != null) {
+                numOfHoles++;
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error!");
         }
-        numOfHoles = numOfHoles-3;
+        numOfHoles = numOfHoles - 3;
         return numOfHoles;
     }
 
     public static int parForHole(int hole) {
         //This method returns the par number for the requested hole
-        int par = 4;
+        int par=0;
+        int i = 0;
+        Map<String, String> holePar = new HashMap<String, String>();
+        String s;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Golf.txt"));
+            while ((s = reader.readLine()) != null) {
+                i++;
+                if (i > 3) {
+                        String[] pairs = s.split(",");
+                        holePar.put(pairs[0], pairs[1]);
+                }
+            }
+            String value = holePar.get(String.valueOf(hole));
+            par = Integer.parseInt(value);
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
         return par;
     }
 
--- MiniGolf/src/minigolf/Main.java
+++ MiniGolf/src/minigolf/Main.java
@@ -28,7 +28,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
     }
     
     public static Round newRound ( String courseName, String playerName){
